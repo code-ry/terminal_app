a52f5b2cc4a73a402bbf4fec9570bbc823fe9ae8
code-ry <rybussey@gmail.com>
2022-09-20 13:58:57 +1000

    Battle feature added to Character class

--- src/main.py
+++ src/main.py
@@ -3,11 +3,12 @@ import storyline as s
 level_one = s.StoryChoice("Want to go Left or Right", 'left')
 level_two = s.StoryChoice("Second choice Want to go Left or Right", 'right')
 player = s.Character()
+dragon = s.Monster('Dragon', 100, 10)
 # play_again = level_one.choice()
 
 def main():
     player.customize()
-    print(player.__repr__)
+    player.battle(dragon)
     # level_one.choice()
     # if level_one.progress == 'win':
     #     level_two.choice()
--- src/storyline.py
+++ src/storyline.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class StoryChoice:
     def __init__(self, question, answer):
         self.question = question
@@ -12,15 +14,10 @@ class StoryChoice:
             self.progress = 'win'
         else:
             print("You Lose")
+
 class Character:
-    # def __init__(self):
-    #     self.name = ''
     hero_list = ['Warrior', 'Hunter', 'Mage']
-    #     self.hero = ''
-    #     self.hp = 0
-    #     self.attack = 10
-    #     self.weapon = ''
-    
+
     def customize(self):
         self.name = input('What is your name?')
         self.hero = input('Which hero would you like to play as? Warrior, Hunter or Mage...')
@@ -46,3 +43,32 @@ class Character:
 
     def __repr__(self):
         return f'{self.name}, {self.hero}, {self.hp}, {self.attack}, {self.weapon}'
+
+    def battle(self, enemy):
+        while self.hp > 0 and enemy.hp > 0:
+            player_roll = randint(1,11)
+            print(f'{self.name} rolls a {player_roll}')
+            enemy_roll = randint(1,11)
+            print(f'{enemy.name} rolls a {enemy_roll}')
+            if player_roll > enemy_roll:
+                enemy.hp -= self.attack
+                print(f'You win! enemy loses {self.attack}health.')
+            elif enemy_roll > player_roll:
+                self.hp -= enemy.attack
+                print(f'{enemy.name} wins! you lose {enemy.attack}health.')
+            else:
+                print("It's a Draw")
+        if self.hp > 0:
+            print('You are the winner!')
+        else:
+            print('You are the Loser!')
+        
+        def puzzle(self):
+            
+
+class Monster:
+    def __init__(self, name, hp, attack):
+        self.name = name
+        self.hp = hp
+        self.attack = attack
+
