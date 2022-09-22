c1139ec9d2bde65bafd8475743240e1e3c1eed18
code-ry <rybussey@gmail.com>
2022-09-22 19:51:46 +1000

    Added rock paper scissors feature

--- src/main.py
+++ src/main.py
@@ -5,41 +5,61 @@ import time
 import numpy as np
 
 welcome = s.StoryChoice('''Welcome to GemQuest, make your way through the unknown lands to find the treasure!\n
-                           To quit at any time type 'quit'\n Press Enter to begin...''', '', '')
-play_again = s.PlayAgain('Would you like to play again?', 'Yes', 'Y')
-level_one = s.StoryChoice("You see a Dragon ahead, Do you attack?", 'Yes','You try and run but the Dragon hunts you down')
-level_two = s.StoryChoice("You see a mysterious figure ahead do you proceed?", 'Yes', 'The Troll boils you in a pot!')
+                           To quit at any time type 'quit'\n Type 'Start' to begin... ''', 'Start', 'quit', '')
+first_story = s.StoryLine('\nYou begin your quest by heading out of town into the forest.')
+first_story.add_line('Then you come across a cave with a Dragon.\n')
+level_one = s.StoryChoice("Do you attack the Dragon? Y or N: ", 'Y','N', 'The Dragon melts you with his firey breath!')
+second_story = s.StoryLine('You are pleased with yourself that you overcome the Dragon and continue along your path.')
+second_story.add_line('Before long you come across a bridge and begin to cross...')
+second_story.add_line("A Troll jumps out! 'Who dare be crossin' my bridge? Answer 3 O' me riddles correctly and I shall let you past.'")
+level_two = s.StoryChoice('Do you want to answer his riddles? Y or N: ', 'Y', 'N', 'The Troll picks you up and boils you in his pot for dinner!')
+third_story = s.StoryLine("You adopt the nickname of 'Master of Riddles' and continue on your merry way")
+third_story.add_line('On the horizon is a glimmering stone atop a mountain, This must be the Gem you are looking for!')
+third_story.add_line('You almost reach the top before you are stopped by an Ogre...')
+third_story.add_line("'You won\'t be getting your little hands on me gem little one!'")
+third_story.add_line('But I a friendly Ogre so I give you a chance... Lets play a game')
+third_story.add_line('My favourite game, first one to 3 gets the shiney Gem!')
+ending = s.StoryLine('You gleefully skip off with your treasure and return to the Village to show your friends!')
+ending.add_line('THE END')
+
+play_again = s.PlayAgain('Would you like to play again? Y or N: ', 'Y', 'N',)
+
 player = s.Player()
-dragon = s.Monster('Dragon', 100, 10, 'breathes a gigantic Fireball')
-first_story = s.StoryLine('You begin your quest by heading out of town into the forest.')
-first_story.add_line('Then you come across a cave with a dragon.')
+dragon = s.Monster('The Dragon', 100, 10, 'breathes a gigantic Fireball')
+final_boss = s.RockPaperScissors()
 
 def main():
     try:
         while True:
+            player.puzzle()
             art.tprint("GemQuest")
             welcome.choice()
             time.sleep(1)
             if welcome.progress:
                 player.hero_pick()
                 player.set()
+                time.sleep(1)
                 first_story.concat_lines()
                 level_one.choice()
-                time.sleep(1)
                 if level_one.progress:
                     player.battle(dragon)
-                    time.sleep(1)
                     if player.hp > 0:
+                        second_story.concat_lines()
                         level_two.choice()
                         time.sleep(1)
                         if level_two.progress:
                             player.puzzle()
+                            if player.master_of_riddles:
+                                third_story.concat_lines()
+                                final_boss.game(player)
+                                if player.has_gem:
+                                    ending.concat_lines()
                         else:
-                            print(level_two.fail)
+                            print(level_two.death_message)
                     else:
-                        print(level_one.fail)
+                        print(level_one.death_message)
                 else:
-                    print(level_one.fail)
+                    print(level_one.death_message)
             play_again.choice()
     except KeyboardInterrupt:
         print("Thanks for playing! GoodBye") 
--- src/storyline.py
+++ src/storyline.py
@@ -11,19 +11,40 @@ def get_input(prompt):
     return input_value
 
 class StoryChoice:
-    def __init__(self, question, answer, fail, alt = 'Y'):
+    def __init__(self, question, path_one, path_two, death_message):
         self.question = question
-        self.answer = answer
+        self.path_one = path_one
+        self.path_two = path_two
         self.progress = True
-        self.alt = alt
-        self.fail = fail
+        self.death_message = death_message
 
     def choice(self):
-        answer = get_input(f'{self.question}')
-        if answer in (self.answer, self.alt):
+        user_input = get_input(f'{self.question}')
+        while user_input not in (self.path_one, self.path_two):
+            print(f'Please enter either {self.path_one} or {self.path_two}')
+            user_input = get_input(f'{self.question}')
+        if user_input == self.path_one:
             self.progress = True
         else:
             self.progress = False
+
+class PlayAgain(StoryChoice):
+    def __init__(self, question, path_one, path_two):
+        self.question = question
+        self.path_one = path_one
+        self.path_two = path_two
+        self.progress = True
+        
+    def choice(self):
+        user_input = get_input(f'{self.question}')
+        while user_input not in (self.path_one, self.path_two):
+            print(f"Please enter either {self.path_one} or {self.path_two} or 'quit'.a")
+            user_input = get_input(f'{self.question}')
+        if user_input == self.path_one:
+            self.progress = True
+        else:
+            raise KeyboardInterrupt
+
 class StoryLine:
     def __init__(self, line):
         self.line = [line]
@@ -36,32 +57,24 @@ class StoryLine:
             print(i)
             time.sleep(2)
 
-
-class PlayAgain(StoryChoice):
-    def choice(self):
-        answer = get_input(f'{self.question}')
-        if answer in (self.answer, self.alt):
-            self.progress = True
-        else:
-            raise KeyboardInterrupt
-
-
 class Player:
     hero_list = np.array(['a', 'b', 'c'])
     hero = ''
     items = {'Ring of health' : 20, 'Ring of Attack' : 40}
+    master_of_riddles = False
+    has_gem = False
 
     def set(self):
         if self.hero == 'a':
             self.name = 'Grimly The Fierce'
             self.hp = 100
-            self.attack = 10
+            self.attack = 20
             self.weapon = 'swings their mighty Axe'
 
         elif self.hero == 'b':
             self.name = 'Ethondril the Agile'
             self.hp = 60
-            self.attack = 20
+            self.attack = 30
             self.weapon = 'fires their Long-Bow'
 
         else:
@@ -71,21 +84,20 @@ class Player:
             self.weapon = 'launches a devestating Frostbolt'
 
     def hero_pick(self):
-
+        self.hero = get_input('Which hero would you like to play as?\n a. Grimly The Fierce a Warrior\n b. Ethondril the Agile an Archer\n c. Ailwyn the Powerful a Wizard\n')
         while self.hero not in self.hero_list:
-                
-                self.hero = get_input('Which hero would you like to play as?\n a. Grimly The Fierce a Warrior\n b. Ethondril the Agile an Archer\n c. Ailwyn the Powerful a Wizard\n')
+            self.hero = get_input('Please enter either a, b or c: ')          
 
     def __repr__(self):
         return f'{self.hero}, {self.hp}, {self.attack}, {self.weapon}'
 
     def battle(self, enemy):
-        get_input(f'You face off with the {enemy.name}.\n You must roll a higher number to attack. Press enter to roll!')
+        get_input(f'\nYou face off with the {enemy.name}.\n You must roll a higher number than {enemy.name} to attack. Press enter to roll!\n')
         counter = 0
         enemy.hp = enemy.base_hp
         while self.hp > 0 and enemy.hp > 0:
             if counter > 0:
-                get_input('Press enter to Roll again!')
+                get_input('\nPress enter to Roll again!')
             counter += 1
             time.sleep(1)
             player_roll = randint(1,10)
@@ -96,41 +108,66 @@ class Player:
             time.sleep(1)
             if player_roll > enemy_roll:
                 enemy.hp -= self.attack
-                print(f'{self.name} attacks and {self.weapon}.\n The {enemy.name} loses {self.attack} health with {enemy.hp} remaining.')
+                print(f'{self.name} attacks and {self.weapon}.\n')
+                time.sleep(1)
+                print(f'{enemy.name} loses {self.attack} health with {enemy.hp} remaining.')
             elif enemy_roll > player_roll:
                 self.hp -= enemy.attack
-                print(f'{enemy.name} attacks and {enemy.weapon}.\n {self.name} loses {enemy.attack} health with {self.hp} remaining.')
+                print(f'{enemy.name} attacks and {enemy.weapon}.\n')
+                time.sleep(1)
+                print(f'{self.name} loses {enemy.attack} health with {self.hp} remaining.')
             else:
                 print('It\'s a Draw, roll again!')
             time.sleep(1)
         if self.hp > 0:
-            print('You are the winner!')
-            
-        else:
-            print('You are the Loser!')
-        
+            print(f'You defeat {enemy.name}!!!')
+            time.sleep(2)
+     
     def puzzle(self):
-        riddles = np.array([
-            'What has to be broken before you can use it?',
-            'I\'m tall when young yet short when I\'m old, What am I?',
-            'I\'m full of holes but still hold water?'
-            ])
-        clues = np.array([
+        riddles =[
+            'What has to be broken before you can use it?\n',
+            'I\'m tall when young yet short when I\'m old, What am I?\n',
+            'I\'m full of holes but still hold water?\n',
+            'What has lots of eyes, but can\'t see?\n',
+            'I am always hungry and will die if not fed, but whatever I touch will soon turn red. What am I?\n'
+            ]
+        clues = [
             'Who first, me or the Chicken!',
             'I show you the way in the dark!',
-            'My best friend is Patrick!'
-        ])
-        answers = ['Egg', 'Candle', 'Sponge']
-        attempts = 5
-        x = randint(0, 2)
-        while attempts > 0:
-            player_answer = get_input(riddles[x])
-            if player_answer in answers[x]:
-                print('You Win!')
-                break
-            attempts -= 1
-            if attempts < 3:
-                print(f'Ok I will give you a clue... {clues[x]}')
+            'My best friend is Patrick!',
+            'You can cut me into chips',
+            'I keep you warm at night and cook your food'
+        ]
+        answers = ['Egg', 'Candle', 'Sponge', 'Potato', 'Fire']
+        attempts = 0
+        wins = 0
+        i = randint(0, len(riddles) - 1)
+        while attempts < 5 and wins < 3:
+            print(f'You have {5 - attempts} attempts left')
+            time.sleep(1)
+            player_answer = get_input(riddles[i])
+            time.sleep(1)
+            if attempts == 2:
+                print(f'Ok I will give you a clue... \n{clues[i]}')
+                time.sleep(1)
+            if player_answer == answers[i]:
+                print(f'You be clever cookie!')
+                time.sleep(1)
+                wins += 1
+                attempts == 0
+                riddles.pop(i)
+                clues.pop(i)
+                answers.pop(i)
+                i = randint(0, len(riddles) - 1)
+                print(f'You have {wins} riddles solved!')
+            else:
+                attempts += 1
+        if wins == 3:
+            self.master_of_riddles = True
+            print('You be wiser than I thought. Blessed e your travels!')
+        else: 
+            print(f"You not so bright aren't ya, da answer is {answers[i]}")
+            time.sleep(2)
 
 class Monster:
     def __init__(self, name, base_hp, attack, weapon):
@@ -140,4 +177,52 @@ class Monster:
         self.weapon = weapon
         self.hp = base_hp
 
-    
+class RockPaperScissors:
+    def game(self, player):
+        player_wins = 0
+        comp_wins = 0
+        time.sleep(1)
+        while player_wins != 3 and comp_wins != 3:
+            user_input = get_input('Lets play.. Rock, Paper or Scissors?: ')
+            while user_input not in ('Rock', 'Paper', 'Scissors'):
+                print('Please enter either Rock, Paper or Scissors')
+                user_input = get_input('')
+            random_int = randint(1,3)
+            if random_int == 1:
+                comp_choice = 'Rock'
+            elif random_int == 2:
+                comp_choice = 'Paper'
+            else:
+                comp_choice = 'Scissors'
+            time.sleep(1)
+            print(f'Ogre chooses {comp_choice}')
+            time.sleep(1)
+            if user_input == comp_choice:
+                print('Its a tie, Go again!')
+                continue
+            if user_input == 'Rock' and comp_choice == 'Paper':
+                player_wins += 1
+                print('You win this round!')
+            if user_input == 'Rock' and comp_choice == 'Scissors':
+                comp_wins += 1
+                print('AHA I win this one!')
+            if user_input == 'Scissors' and comp_choice == 'Paper':
+                player_wins += 1
+                print('You are better than I thought!')
+            if user_input == 'Scissors' and comp_choice == 'Rock':
+                comp_wins += 1
+                print('I am so smart.. S M R T...')
+            if user_input == 'Paper' and comp_choice == 'Rock':
+                player_wins += 1
+                print('You must be cheating!')
+            if user_input == 'Paper' and comp_choice == 'Scissors':
+                comp_wins += 1
+            time.sleep(1)
+            print(f'Your wins: {player_wins}\nOgre wins: {comp_wins}')
+        if player_wins == 3:
+            print('I don\'t know how but you beat me!')
+            player.has_gem = True
+        else:
+            print('Come back when you\'ve had some practice, NO gem for you!')
+
+        
